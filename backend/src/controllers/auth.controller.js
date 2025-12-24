dc5662ae001e6ab668a482e5ac700da66ff05795
syedaftab-dev <syed.mohd.aftab.2027@gmail.com>
2025-12-24 22:56:20 +0530

    basic auth routes created

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export async function signup(req,res){
+    res.send("sigup page")
+}
+
+export async function login(req,res){
+    res.send("login page")
+
+}
+
+export async function logout(req,res){
+    res.send("logout page")
+
+}
---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+import authRoutes from "./routes/auth.route.js"
+
+const app = express()
+const port = process.env.PORT || 5001;
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
