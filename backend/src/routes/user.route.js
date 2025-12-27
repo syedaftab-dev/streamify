4e6906cbc15c19f10f41dcd5f03bf26c7bda3cea
syedaftab-dev <syed.mohd.aftab.2027@gmail.com>
2025-12-27 23:37:24 +0530

    friend request endpoint

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,18 +1,26 @@
-import express from "express"
+import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getRecommendedUsers, getMyFriends } from "../controllers/user.controller.js";
-import { sendFriendRequest } from "../controllers/user.controller.js";
+import {
+  acceptFriendRequest,
+  getFriendRequests,
+  getMyFriends,
+  getOutgoingFriendReqs,
+  getRecommendedUsers,
+  sendFriendRequest,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+// apply auth middleware to all routes
 router.use(protectRoute);
 
-router.get("/",getRecommendedUsers);
-
-router.get("/friends",getMyFriends);
+router.get("/", getRecommendedUsers);
+router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
-router.put()
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
